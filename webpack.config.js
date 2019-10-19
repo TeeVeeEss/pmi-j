938fd52f9e34b79ccd4755db478208db41217fe9
TeeVeeEss <TeeVeeEss@users.noreply.github.com>
2019-10-19 16:25:58 +0200

    finetune webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,9 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     host: '0.0.0.0',
-    port: 9876
+    port: 9876,
+    disableHostCheck: true,
+    historyApiFallback: true
   },
   plugins: [
     new CleanWebpackPlugin(),
